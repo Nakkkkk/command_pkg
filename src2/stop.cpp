c0ee2e310ce94858e8b62711512123792ed3531e
Nakkkkk <p238046k@mail.kyutech.jp>
2018-08-04 03:28:29 +0900

    2018/8/2 17:00

--- src2/stop.cpp
+++ src2/stop.cpp
@@ -72,10 +72,10 @@ public:
 			if(latest_scan.ranges.size() > 0)
 			{
 //				geometry_msgs::Twist cmd_vel;
-				int size = 2*(-latest_scan.angle_min) / latest_scan.angle_increment;
-				int i = size/2;
-				int j = 3*size/8;
-				int k = 5*size/8;
+				int size = (latest_scan.angle_max) / latest_scan.angle_increment;
+				int i = 0;
+				int j = 1*size/8;
+				int k = 7*size/8;
 				if(latest_scan.ranges[i] < latest_scan.range_min ||
 						latest_scan.ranges[i] > latest_scan.range_max || 
 						std::isnan(latest_scan.ranges[i]))
@@ -92,12 +92,13 @@ public:
 						cmd_vel.linear.x = 0.1;
 						cmd_vel.angular.z = 0.0;
 					}
-					else if(latest_scan.ranges[k] < 0.5)
+
+					if(latest_scan.ranges[j] < 0.4)
 					{
 						cmd_vel.linear.x = 0.0;
 						cmd_vel.angular.z = -0.8;
 					}
-					else if(latest_scan.ranges[j] < 0.5)
+					else if(latest_scan.ranges[k] < 0.4)
 					{
 						cmd_vel.linear.x = 0.0;
 						cmd_vel.angular.z = 0.8;
